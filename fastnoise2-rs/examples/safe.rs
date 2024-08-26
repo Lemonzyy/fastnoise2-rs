fd973eef50e0a51a566a47de0683e6b2eff6fbbc
Lemonzy <lemonzy.pro@gmail.com>
2024-08-26 23:45:24 +0200

    Remove unused example

--- fastnoise2-rs/examples/safe.rs
+++ fastnoise2-rs/examples/safe.rs
@@ -7,7 +7,6 @@ use fastnoise2::{
         blend::{Add, Fade},
         prelude::*,
         simplex::Simplex,
-        Generator,
     },
     SafeNode,
 };
@@ -111,93 +110,3 @@ fn save(img: GrayImage, filename: &str) {
     img.save(&output_path).expect("Failed to save image");
     println!("Image successfully saved as {}", output_path.display());
 }
-
-// This example illustrates the use of different types of writing with generators to build safe trees.
-// use std::time::Instant;
-
-// use fastnoise2::{
-//     gen::{checkerboard, FractalFBm, Generator, GeneratorWrapper, Perlin},
-//     SafeNode,
-// };
-// use image::{GrayImage, Luma};
-
-// const X_SIZE: i32 = 2048;
-// const Y_SIZE: i32 = 2048;
-
-// fn create_node() -> GeneratorWrapper<SafeNode> {
-//     // Add {
-//     //     lhs: Checkerboard { size: 1.0 },
-//     //     rhs: SineWave { scale: 2.0 },
-//     // }
-//     // .build()
-//     let checkerboard = checkerboard(5.0).build();
-//     (GeneratorWrapper(FractalFBm {
-//         source: Perlin,
-//         gain: 0.65,
-//         weighted_strength: checkerboard.clone() + 0.5,
-//         octaves: 4,
-//         lacunarity: 0.5,
-//     }) + checkerboard)
-//         .build()
-// }
-
-// fn main() {
-//     #[cfg(feature = "trace")]
-//     {
-//         use tracing_subscriber::layer::SubscriberExt;
-//         tracing::subscriber::set_global_default(
-//             tracing_subscriber::registry().with(tracing_tracy::TracyLayer::default()),
-//         )
-//         .unwrap();
-//         std::thread::sleep(std::time::Duration::from_secs(1));
-//     }
-
-//     let node = create_node();
-
-//     println!("SIMD level: {}", node.get_simd_level());
-
-//     let mut noise = vec![0.0; (X_SIZE * Y_SIZE) as usize];
-
-//     let start = Instant::now();
-//     let min_max = node.gen_uniform_grid_2d(
-//         &mut noise,
-//         -X_SIZE / 2,
-//         -Y_SIZE / 2,
-//         X_SIZE,
-//         Y_SIZE,
-//         0.02,
-//         1337,
-//     );
-//     let elapsed = start.elapsed();
-
-//     println!(
-//         "Took {elapsed:?} to generate {} values ({}/s): {min_max:?}",
-//         noise.len(),
-//         noise.len() as f32 / elapsed.as_secs_f32()
-//     );
-
-//     // Do whatever you want with `noise`! In this case, generate an image with it.
-
-//     let mut img = GrayImage::new(X_SIZE as u32, Y_SIZE as u32);
-
-//     for x in 0..X_SIZE {
-//         for y in 0..Y_SIZE {
-//             let index = ((Y_SIZE - 1 - y) * X_SIZE + x) as usize;
-//             let value = noise[index];
-//             let pixel_value = (255.0 * ((value + 1.0) / 2.0)) as u8;
-//             img.put_pixel(x as u32, y as u32, Luma([pixel_value]));
-//         }
-//     }
-
-//     save(img, "safe.png");
-// }
-
-// fn save(img: GrayImage, filename: &str) {
-//     let output_dir =
-//         std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap_or_default())
-//             .join("examples_output");
-//     std::fs::create_dir_all(&output_dir).expect("Failed to create directories");
-//     let output_path = output_dir.join(filename);
-//     img.save(&output_path).expect("Failed to save image");
-//     println!("Image successfully saved as {}", output_path.display());
-// }
